1a506fbfca3e9c8206dff9e3dcfe098445b674bf
Varun271 <65506707+Varun271@users.noreply.github.com>
2020-08-06 18:35:09 -0300

    Updated Commit

--- sugar/index.js
+++ sugar/index.js
@@ -112,7 +112,38 @@ app.post('/updatesugars', function (req, res) {
     });
 
 });
+app.post('/changeQuantity',function(req,res){
+    
+update(req,res)
 
+});
+
+
+function update(req,res){
+    var type= req.body.type
+    var query1 = `Select  * from sugars where sugar_type = "${type}" `
+    console.log(query1)
+    
+    con.query(query1,(err,result)=>{
+        if(err) throw err
+        if(result.length > 0){
+            const difference = result[0].qty - req.body.suagr_qty_ordered;
+            if(difference<0){
+                res.json({error:"Not enough Quantity"})
+            }else{
+                var query2=`Update sugars set qty = ${difference} where id = ${result[0].id} and sugar_type = "${result[0].sugar_type }"`
+                con.query(query2,(err,response)=>{
+                    if(err) throw err
+                    else{
+                        res.send(response);
+                    }
+                })
+            }
+        }else{
+            res.json({error:"The item is not available"})
+        }
+    })
+}
 
 app.listen(3000, function () {
     console.log("App is running on port 3000");
