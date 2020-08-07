e65c7f73258753ff82b75c56c3c0e4d6425b51f4
Hirenkumar Khant <hr266981@dal.ca>
2020-08-06 22:08:29 -0300

    updated cream route for update quantity

--- cream/backend/routes/creamRoute.js
+++ cream/backend/routes/creamRoute.js
@@ -14,5 +14,10 @@ router.get("/getcreambypk", creamController.getCreamByPk);
 router.post("/addcream", creamController.addCream);
 //to update qty of cream by its pk
 router.put("/updatecream/:id/:type/:qty", creamController.updateCream);
+//to reduce qty of cream
+router.put('/reducecream', async function (req,res){
+    const data = await creamController.reduceCreamQuantity(req);
+    res.send(data);
+})
 
 module.exports = router;
